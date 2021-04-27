9a04d8ae695228c2b3fbfe6b23a2959fd1c90658
Gorshkov Sergey <Gsn82@mail.ru>
2021-04-27 15:09:27 +0500

    Initial commit

---
+++ src/HelloWold.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val test1:String = "Hello"
+    //error val
+    // test1="Goodbye"
+
+    var test2: String = "Hello"
+    test2 = "Goodbye"
+
+    println("Hi jon $test1")
+
+    println("2+2 = $(2+2)")
+}
